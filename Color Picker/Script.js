451d20bf3c0d6245cce7c001e8fa8d4557a8a3d2
Don Quixote <20124537@s.ubaguio.edu>
2025-08-27 13:33:20 +0800

    Add files via upload

---
+++ Color Picker/Script.js
@@ -0,0 +1,14 @@
+const colors = ["Green","Red","Blue","f15025","Yellow"]
+const btn = document.getElementById("btn")
+const color = document.querySelector(".color")
+    
+btn.addEventListener("click", ()=> {
+    console.log(document.body)
+    const randomNumber = getRandomNumber()
+    document.body.style.backgroundColor = colors[randomNumber]
+    color.textContent = colors[randomNumber]
+})
+
+getRandomNumber = () => {
+    return Math.floor(Math.random() * colors.length)
+}
